5f07670175f7cd8e84f4456f46084297611d3a2c
Anastasia Drozdova <avdrozdova3@yandex.ru>
2022-02-01 12:57:33 +0300

    move model init to base trainer

--- train.py
+++ train.py
@@ -1,19 +1,14 @@
 import argparse
 import collections
-import warnings
 
 import numpy as np
-import itertools
 import torch
 
 import src.loss as module_loss
-import src.model as module_arch
 from src.datasets.utils import get_dataloaders
 from src.trainer import Trainer
 from src.utils import prepare_device
 from src.utils.parse_config import ConfigParser
-
-warnings.filterwarnings("ignore", category=UserWarning)
 
 # fix random seeds for reproducibility
 SEED = 123
@@ -24,72 +19,24 @@ np.random.seed(SEED)
 
 
 def main(config):
-    logger = config.get_logger("train")
-
     # setup data_loader instances
     dataloaders = get_dataloaders(config)
-
-    gen_B = config.init_obj(config["generator"], module_arch)
-    gen_A = config.init_obj(config["generator"], module_arch)
-
-    logger.info(gen_A)
 
     # prepare for (multi-device) GPU training
     device, device_ids = prepare_device(config["n_gpu"])
 
     loss_module = config.init_obj(config["loss"], module_loss).to(device)
 
-    if loss_module.adversarial:
-        disc_A = config.init_obj(config["discriminator"], module_arch)
-        disc_B = config.init_obj(config["discriminator"], module_arch)
-        logger.info(disc_A)
-    else:
-        disc_A = None
-        disc_B = None
-
-    gen_B = gen_B.to(device)
-    gen_A = gen_A.to(device)
-
-    if loss_module.adversarial:
-        disc_A = disc_A.to(device)
-        disc_B = disc_B.to(device)
-
-    if len(device_ids) > 1:
-        gen_B = torch.nn.DataParallel(gen_B, device_ids=device_ids)
-        gen_A = torch.nn.DataParallel(gen_A, device_ids=device_ids)
-        if loss_module.adversarial:
-            disc_A = torch.nn.DataParallel(disc_A, device_ids=device_ids)
-            disc_B = torch.nn.DataParallel(disc_B, device_ids=device_ids)
-
-    trainable_params = filter(lambda p: p.requires_grad, itertools.chain(gen_A.parameters(), gen_B.parameters()))
-    optimizer_G = config.init_obj(config["optimizer"], torch.optim, trainable_params)
-
-    if loss_module.adversarial:
-        trainable_params = filter(lambda p: p.requires_grad, disc_A.parameters())
-        optimizer_DA = config.init_obj(config["optimizer"], torch.optim, trainable_params)
-
-        trainable_params = filter(lambda p: p.requires_grad, disc_B.parameters())
-        optimizer_DB = config.init_obj(config["optimizer"], torch.optim, trainable_params)
-    else:
-        optimizer_DA, optimizer_DB = None, None
-
-    # lr_scheduler = config.init_obj(config["lr_scheduler"], torch.optim.lr_scheduler, optimizer)
-
     trainer = Trainer(
-        gen_B,
-        gen_A,
-        disc_A,
-        disc_B,
         loss_module,
-        optimizer_G,
-        optimizer_DA,
-        optimizer_DB,
         config=config,
         device=device,
+        device_ids=device_ids,
         data_loader_A=dataloaders["train_loader_A"],
         data_loader_B=dataloaders["train_loader_B"],
         valid_data_loader_A=dataloaders["val_loader_A"],
-        valid_data_loader_B=dataloaders["val_loader_B"]
+        valid_data_loader_B=dataloaders["val_loader_B"],
+        adversarial=loss_module.adversarial,
     )
 
     trainer.train()

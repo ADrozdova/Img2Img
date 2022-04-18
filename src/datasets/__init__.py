bc12934c87eddbbda2c34c9b0fdd908b5dd20937
Anastasia Drozdova <avdrozdova3@yandex.ru>
2022-04-18 17:32:44 +0300

    fix init

--- src/datasets/__init__.py
+++ src/datasets/__init__.py
@@ -1,6 +1,5 @@
 from src.datasets.dataset import ImageFolderDataset
 from src.datasets.pascal_voc import PascalVOCDataset
-from src.datasets.builder import build_loader, build_text_transform
 
 from .builder import build_loader, build_text_transform
 from .imagenet_template import imagenet_classes, template_meta
